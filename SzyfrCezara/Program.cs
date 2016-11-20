dcf9f2f2c5528a8244d00c5cb4035cd9862e615a
ja <ja@JA-KOMPUTER>
2016-11-20 20:51:47 +0100

    first

---
+++ SzyfrCezara/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SzyfrCezara
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           Console.WriteLine("Wprowadź hasło, które chcesz zmienić na szyf Cezara: ");
+            string kodZnaku = Console.ReadLine();
+            char[] chars = kodZnaku.ToCharArray();
+            for (int i = 0; i < chars.Length; i++)
+            {
+                int szyfr = chars[i] + 3;
+                Console.Write(Convert.ToChar(szyfr));
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
